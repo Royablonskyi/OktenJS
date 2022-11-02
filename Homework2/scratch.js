d34cf26e0550af1f76c5c238fb5d1faa6c742443
Royablonskyi <royablonsky@gmail.com>
2022-11-02 22:17:01 +0200

    Homework2 done

---
+++ Homework2/scratch.js
@@ -0,0 +1,60 @@
+let abc = [15, 1, 5, -25, 'name', true, false, 'test', 99, 'last'];
+console.log(abc);
+
+let book1 = {
+    title: 'Beowulf',
+    pageCount: 150,
+    genre: 'novel',
+};
+let book2 = {
+    title: 'Dark',
+    pageCount: 300,
+    genre: 'horror',
+};
+let book3 = {
+    title: 'Something',
+    pageCount: 15,
+    genre: 'short novel',
+};
+
+let book4 = {
+    title: '1984',
+    pageCount: 500,
+    genre: 'Novel',
+    authors: ['name', 'age'],
+};
+let book5 = {
+    title: 'Sherlock Holmes',
+    pageCount: 200,
+    genre: 'Detective novel',
+    authors: ['name', 'age'],
+};
+let book6 = {
+    title: 'IT',
+    pageCount: 204,
+    genre: 'Horror',
+    authors: ['name', 'age'],
+};
+
+let user = [
+    {name: 'user1', username: 'user_1', password: 'pass1'},
+    {name: 'user2', username: 'user_2', password: 'pass2'},
+    {name: 'user3', username: 'user_3', password: 'pass3'},
+    {name: 'user4', username: 'user_4', password: 'pass4'},
+    {name: 'user5', username: 'user_5', password: 'pass5'},
+    {name: 'user6', username: 'user_6', password: 'pass6'},
+    {name: 'user7', username: 'user_7', password: 'pass7'},
+    {name: 'user8', username: 'user_8', password: 'pass8'},
+    {name: 'user9', username: 'user_9', password: 'pass9'},
+    {name: 'user10', username: 'user_10', password: 'pass10'},
+];
+console.log(user[0].password)
+console.log(user[1].password)
+console.log(user[2].password)
+console.log(user[3].password)
+console.log(user[4].password)
+console.log(user[5].password)
+console.log(user[6].password)
+console.log(user[7].password)
+console.log(user[8].password)
+console.log(user[9].password)
